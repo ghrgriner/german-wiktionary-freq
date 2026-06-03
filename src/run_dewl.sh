23aa0de5d7aeb3ff6b8a65064e485ac4fd04424e
ghrgriner <rgriner_fwd@outlook.com>
2026-06-03 09:07:55 -0500

    Update with new filenames (_dewk.py instead of .py)

--- src/run_dewl.sh
+++ src/run_dewl.sh
@@ -1,7 +1,7 @@
 # only need to do this once to get the corpus
 ###python3 download_sample.py
 
-python3 extract_rwords.py
-python3 reformat.py
-python3 run_wp2022_counts.py
-python3 postprocess.py
+python3 extract_rwords_dewl.py
+python3 reformat_dewl.py
+python3 run_counts_dewl.py
+python3 postprocess_dewl.py
